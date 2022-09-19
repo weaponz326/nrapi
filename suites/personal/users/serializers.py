865cb2091ccd7ebb8225aea84092607fe2d41ea4
Weps <weaponz326@gmail.com>
2022-09-19 21:46:01 +0000

    .

--- suites/personal/users/serializers.py
+++ suites/personal/users/serializers.py
@@ -3,35 +3,18 @@ from rest_framework import serializers
 from .models import CustomBaseModel, User
 
 
-# # TODO: custom base serializer not working
-# class CustomBaseSerializer(serializers.ModelSerializer):
-#     class Meta:
-#         model = CustomBaseModel
-#         fields = ['id', 'created_at', 'updated_at']
-
-# class UserSerializer(CustomBaseSerializer):
-#     class Meta:
-#         model = User
-#         fields = [
-#             'email',
-#             'first_name',
-#             'last_name',
-#             'location',
-#             'about',
-#             'photo'
-#         ]
-
 class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = [
-            'id',
-            'created_at',
-            'updated_at',
-            'email',
-            'first_name',
-            'last_name',
-            'location',
-            'about',
-            'photo'
-        ]
+        fields = '__all__'
+        # fields = [
+        #     'id',
+        #     'created_at',
+        #     'updated_at',
+        #     'email',
+        #     'first_name',
+        #     'last_name',
+        #     'location',
+        #     'about',
+        #     'photo'
+        # ]
