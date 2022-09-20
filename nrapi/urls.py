4f86f9b55c1ff8253c0d1da95e4b5a68db7862f9
Weps <weaponz326@gmail.com>
2022-09-20 11:42:23 +0000

    .

--- nrapi/urls.py
+++ nrapi/urls.py
@@ -30,11 +30,11 @@ urlpatterns = [
 
     # personal
     path('personal-users/', include('suites.personal.users.urls')),
-    path('personal-nodules/calendar/', include('suites.personal.modules.calendarx.urls')),
-    path('personal-nodules/budget/', include('suites.personal.modules.budget.urls')),
-    path('personal-nodules/notes/', include('suites.personal.modules.notes.urls')),
-    path('personal-nodules/accounts/', include('suites.personal.modules.accountx.urls')),
-    path('personal-nodules/tasks/', include('suites.personal.modules.tasks.urls')),
-    path('personal-nodules/portal/', include('suites.personal.modules.portal.urls')),
-    path('personal-nodules/settings/', include('suites.personal.modules.settings.urls')),
+    path('personal-modules/calendar/', include('suites.personal.modules.calendarx.urls')),
+    path('personal-modules/budget/', include('suites.personal.modules.budget.urls')),
+    path('personal-modules/notes/', include('suites.personal.modules.notes.urls')),
+    path('personal-modules/accounts/', include('suites.personal.modules.accountx.urls')),
+    path('personal-modules/tasks/', include('suites.personal.modules.tasks.urls')),
+    path('personal-modules/portal/', include('suites.personal.modules.portal.urls')),
+    path('personal-modules/settings/', include('suites.personal.modules.settings.urls')),
 ]
