04f3c5a76e6ec9023a0a36f8510f8da76f67a24f
Weps <weaponz326@gmail.com>
2022-09-22 07:25:20 +0000

    .

--- nrapi/urls.py
+++ nrapi/urls.py
@@ -16,6 +16,9 @@ Including another URLconf
 
 from django.contrib import admin
 from django.urls import include, path
+from django.conf.urls.static import static
+from django.conf import settings
+
 from rest_framework import routers
 
 router = routers.DefaultRouter()
@@ -54,3 +57,5 @@ urlpatterns = [
     path('restaurant-modules/reservations/', include('suites.restaurant.modules.reservations.urls')),
     path('restaurant-modules/customers/', include('suites.restaurant.modules.customers.urls')),
 ]
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
