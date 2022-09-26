d4445953931cb49fadfb5c21d31f74550211888e
Weps <weaponz326@gmail.com>
2022-09-26 07:41:07 +0000

    excluded self from search querys

--- suites/personal/users/views.py
+++ suites/personal/users/views.py
@@ -13,13 +13,26 @@ from .paginations import TablePagination
 # Create your views here.
 
 # user search
-class UserSearchView(generics.ListAPIView):
-    queryset = User.objects.all()
-    serializer_class = UserSerializer
+# class UserSearchView(generics.ListAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserSerializer
+#     pagination_class = TablePagination
+#     filter_backends = [filters.SearchFilter]
+#     search_fields = ['first_name', 'last_name']
+
+class UserSearchView(APIView, TablePagination):
+    permission_classes = (IsAuthenticated,)
     pagination_class = TablePagination
     filter_backends = [filters.SearchFilter]
     search_fields = ['first_name', 'last_name']
 
+    def get(self, request, format=None):
+        user = self.request.query_params.get('user', None)
+        user = User.objects.all().exclude(id=user)
+        results = self.paginate_queryset(user, request, view=self)
+        serializer = UserSerializer(results, many=True)        
+        return self.get_paginated_response(serializer.data)
+      
 class UserDetailView(APIView):
     permission_classes = (IsAuthenticated,)
 
