76a049307ac3230afaf99c8d3026bd915247a5e7
Weps <weaponz326@gmail.com>
2022-09-28 16:54:33 +0000

    generated payments app in personal

--- suites/restaurant/modules/payments/apps.py
+++ suites/restaurant/modules/payments/apps.py
@@ -4,4 +4,5 @@ from django.apps import AppConfig
 class PaymentsConfig(AppConfig):
     default_auto_field = 'django.db.models.BigAutoField'
     name = 'suites.restaurant.modules.payments'
+    label = 'restaurant_module_payments'
     verbose_name = 'restaurant module payments'
