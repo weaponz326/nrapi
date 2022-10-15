9c8709f09b85d798b4eb9339da6a834efd062aa3
Weps <weaponz326@gmail.com>
2022-10-15 13:29:09 +0000

    school teachers module

--- suites/school/modules/teachers/admin.py
+++ suites/school/modules/teachers/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Teacher, TeacherCodeConfig
+
+
 # Register your models here.
+
+class TeacherAdmin(admin.ModelAdmin):
+    list_display = ('pkid', 'id', 'created_at', 'first_name', 'last_name', 'account', 'teacher_code')
+
+class TeacherCodeConfigAdmin(admin.ModelAdmin):
+    list_display = ('pkid', 'id', 'created_at', 'prefix', 'suffix', 'year_code')
+
+admin.site.register(Teacher, TeacherAdmin)
+admin.site.register(TeacherCodeConfig, TeacherCodeConfigAdmin)
--- suites/school/modules/teachers/urls.py
+++ suites/school/modules/teachers/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+
 from . import views
 
-urlpatterns = [
 
+urlpatterns = [
+    path('teacher/', views.TeacherView.as_view()),
+    path('teacher/<id>', views.TeacherDetailView.as_view()),
+
+    path('config/teacher-code/<id>', views.TeacherCodeConfigDetailView.as_view()),
+    path('config/new-teacher-code/<id>', views.NewTeacherCodeConfigView.as_view()),
 ]
