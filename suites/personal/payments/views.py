041750d62e5c031aa2bf9dd7ebed6fe72bc45e80
Weps <weaponz326@gmail.com>
2022-10-16 20:29:05 +0000

    latest

--- suites/personal/payments/views.py
+++ suites/personal/payments/views.py
@@ -56,7 +56,7 @@ def process_webhook_payload(payload):
 
     # subscription charged
     elif payload['event'] == 'charge.success':
-        subscription = Subscription.objects.filter(email=email, status='Pending').last()
+        subscription = Subscription.objects.filter(email=email, status='Pending').latest('created_at')
         subscription.update(
             customer_code=customer_code,
             status='Active', 
