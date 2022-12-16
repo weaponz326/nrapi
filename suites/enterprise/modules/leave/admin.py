a47b1f3aefcd6263b3bb666c25a414ea8bdb860c
Weps <weaponz326@gmail.com>
2022-12-16 05:57:29 +0000

    enterprise assets module

--- suites/enterprise/modules/leave/admin.py
+++ suites/enterprise/modules/leave/admin.py
@@ -1,3 +1,11 @@
 from django.contrib import admin
 
+from .models import Leave
+
+
 # Register your models here.
+
+class LeaveAdmin(admin.ModelAdmin):
+    list_display = ('pkid', 'id', 'created_at', 'leave_code', 'leave_type', 'leave_status')
+
+admin.site.register(Leave, LeaveAdmin)
