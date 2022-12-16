1133eb816c3dbd6f107800d23d9dbe55b5277365
Weps <weaponz326@gmail.com>
2022-12-16 19:16:59 +0000

    enterprise appraisal module

--- suites/enterprise/modules/appraisal/admin.py
+++ suites/enterprise/modules/appraisal/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
 
+from .models import Appraisal, AppraisalSheet
+
+
 # Register your models here.
+
+class AppraisalAdmin(admin.ModelAdmin):
+    list_display = ('pkid', 'id', 'created_at', 'appraisal_code', 'appraisal_name')
+
+class AppraisalSheetAdmin(admin.ModelAdmin):
+    list_display = ('pkid', 'id', 'created_at')
+
+admin.site.register(Appraisal, AppraisalAdmin)
+admin.site.register(AppraisalSheet, AppraisalSheetAdmin)
--- suites/enterprise/modules/appraisal/models.py
+++ suites/enterprise/modules/appraisal/models.py
@@ -1,3 +1,35 @@
 from django.db import models
 
+from suites.personal.users.models import CustomBaseModel
+from suites.enterprise.accounts.models import Account
+
+
 # Create your models here.
+
+class Appraisal(CustomBaseModel):
+    account = models.ForeignKey(Account, to_field='id', on_delete=models.DO_NOTHING)
+    # employee = models.ForeignKey(Employee, to_field='id', on_delete=models.DO_NOTHING)
+    appraisal_code = models.CharField(max_length=64, null=True, blank=True)
+    appraisal_name = models.CharField(max_length=256, null=True, blank=True)
+    start_date = models.DateField(null=True, blank=True)
+    end_date = models.DateField(null=True, blank=True)
+    suoervisor = models.CharField(max_length=128, null=True, blank=True)
+
+    class Meta:
+        db_table = 'enterprise_module_appraisal'
+
+    def __str__(self):
+        return str(self.id)
+
+class AppraisalSheet(CustomBaseModel):
+    knowledge = models.CharField(max_length=4, null=True, blank=True)
+    quality = models.CharField(max_length=4, null=True, blank=True)
+    productivity = models.CharField(max_length=4, null=True, blank=True)
+    dependability = models.CharField(max_length=4, null=True, blank=True)
+    attendance = models.CharField(max_length=4, null=True, blank=True)
+
+    class Meta:
+        db_table = 'enterprise_module_appraisal_sheet'
+
+    def __str__(self):
+        return str(self.id)
