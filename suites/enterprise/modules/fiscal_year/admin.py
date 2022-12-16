98cde0433a494b856980f5af0a293d8b069fb06f
Weps <weaponz326@gmail.com>
2022-12-16 20:04:35 +0000

    enterprise fiscal year module

--- suites/enterprise/modules/fiscal_year/admin.py
+++ suites/enterprise/modules/fiscal_year/admin.py
@@ -1,3 +1,19 @@
 from django.contrib import admin
 
+from .models import ActiveFiscalYear, FiscalYear, FiscalYearCodeConfig
+
+
 # Register your models here.
+
+class FiscalYearAdmin(admin.ModelAdmin):
+    list_display = ('pkid', 'id', 'created_at', 'account', 'year_code', 'year_name', 'year_status')
+
+class ActiveFiscalYearAdmin(admin.ModelAdmin):
+    list_display = ('pkid', 'id', 'created_at', 'year')
+
+class FiscalYearCodeConfigAdmin(admin.ModelAdmin):
+    list_display = ('pkid', 'id', 'created_at', 'prefix', 'suffix')
+
+admin.site.register(FiscalYear, FiscalYearAdmin)
+admin.site.register(ActiveFiscalYear, ActiveFiscalYearAdmin)
+admin.site.register(FiscalYearCodeConfig, FiscalYearCodeConfigAdmin)
--- suites/enterprise/modules/fiscal_year/urls.py
+++ suites/enterprise/modules/fiscal_year/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
 from . import views
 
+
 urlpatterns = [
-    
+    path('fiscal-year/', views.FiscalYearView.as_view()),
+    path('fiscal-year/<id>', views.FiscalYearDetailView.as_view()),
+    path('active-fiscal-year/<id>', views.ActiveFiscalYearDetailView.as_view()),
+
+    path('config/fiscal-year-code/<id>', views.FiscalYearCodeConfigDetailView.as_view()),
+    path('config/new-fiscal-year-code/<id>', views.NewFiscalYearCodeConfigView.as_view()),
 ]
