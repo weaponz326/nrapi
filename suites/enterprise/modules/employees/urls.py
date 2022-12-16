f631e53fc50cb4127152b01dbac4e8b92e35f0ff
Weps <weaponz326@gmail.com>
2022-12-16 19:37:53 +0000

    enterprise employees module

--- suites/enterprise/modules/employees/urls.py
+++ suites/enterprise/modules/employees/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path, include
+
 from . import views
 
+
 urlpatterns = [
-    
+    path('employee/', views.EmployeeView.as_view()),
+    path('employee/<id>', views.EmployeeDetailView.as_view()),
+
+    path('config/employee-code/<id>', views.EmployeeCodeConfigDetailView.as_view()),
+    path('config/new-employee-code/<id>', views.NewEmployeeCodeConfigView.as_view()),
 ]
